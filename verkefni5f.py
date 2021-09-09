a6c1888b5dcae10329295a09bdddba6473d954c6
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-09 22:58:01 +0000

    Final version

--- verkefni5f.py
+++ verkefni5f.py
@@ -4,3 +4,14 @@ replacement = input("Enter in a substring you want to replace it with: ")
 
 # Complete the program below
 
+#echo "# Timaverkefni5" >> README.md
+#git add README.md
+"""git p
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gudmundurkg/Timaverkefni5.git
+git push -u origin main
+"""
